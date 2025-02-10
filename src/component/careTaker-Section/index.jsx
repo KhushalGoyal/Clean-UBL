1ed6955d5d19e4ffa2bdf1132bb9c88d526cfff8
Javed Shamma <javedshamma@gmail.com>
2025-02-10 23:26:24 +0530

    Updated on 10Feb

--- src/component/careTaker-Section/index.jsx
+++ src/component/careTaker-Section/index.jsx
@@ -1,12 +1,54 @@
 import styles from "./styles.module.css";
 import Btoilet from "../../assets/icons/Btoilet.png"
+
+import { useState } from "react";
 const CareTakerDashboard=()=>{
+    const[isShow,setisShow]=useState(false);
+    const handleShow=()=>{
+        setisShow(!isShow);
+    }
     return(
-       <div className={styles.backGroundArea}>
+    <div className={styles.backGroundArea}>
             <div className={styles.mainArea}>
                 <h3 className="text-primary text-center">Toilets are care taken by Care Takers</h3>
                 <div className={styles.cardSection}>
-                   <div className={styles.card}>
+                   {isShow && <div className={styles.popUpArea}>
+                    <div className={styles.popUp}>
+                        <div className={styles.cardArea}>
+                            <h3 className="txt-center text-primary">
+                                All Details of toilet cleaning and caretaker
+                            </h3>
+                            <div className={styles.detailedContainer}>
+                                <div className={styles.smallCard}>
+                                    <p>Toilet Opening time : 10.00 AM</p>
+                                </div>
+                                <div className={styles.smallCard}>
+                               
+                                </div>
+                                <div className={styles.smallCard}>
+                                
+                                </div>
+                            </div>
+                            <div className="d-flex">
+                                <div className={styles.imageArea}>
+                                    <img className={styles.ctPhotos} src="https://c8.alamy.com/comp/ET0YB2/toilet-for-gents-ladies-public-toilets-man-woman-men-women-plastic-ET0YB2.jpg" alt="" />
+                                    <img className={styles.ctPhotos} src="https://ichef.bbci.co.uk/news/976/cpsprodpb/91C9/production/_130812373_gettyimages-120273953-594x594.jpg" alt="" />
+                                    <img className={styles.ctPhotos} src="https://thevoiceofchandigarh.com/wp-content/uploads/2022/06/IMG_20220606_101125.jpg" alt="" />
+                                    <img className={styles.ctPhotos} src="https://sbmurban.org/storage/app/media/F7H6Ey6WsAECY3f.jpg" alt="" />
+                                    <img className={styles.ctPhotos} src="https://4.imimg.com/data4/WQ/PU/MY-3911057/img-20151029-wa0072.jpg" alt="" />
+                                    <img className={styles.ctPhotos} src="https://content.jdmagicbox.com/v2/comp/chennai/q9/044pxx44.xx44.240810211037.q1q9/catalogue/shirajudin-cleaning-services-pallikaranai-chennai-residential-cleaning-services-71iphomecv.jpg" alt="" />
+                                </div>
+                                <div className={styles.cleaningDetails}>
+
+                                </div>
+                            </div>
+
+                        </div>
+                        <button onClick={handleShow} type="button" id={styles.closeBtn} className="btn btn-primary m-1 p-2">Close</button>
+                    </div>
+                   
+                    </div>
+                    }<div className={styles.card}>
                         <div className={styles.logoArea}>
                             <img className={styles.logoImage} src={Btoilet} alt="" />
                         </div>
@@ -22,7 +64,7 @@ const CareTakerDashboard=()=>{
                             <p>O&M :ULB </p>
                         </div>
                         <div className={styles.cleanAndOpenArea}>
-                            <button type="button" className="btn btn-success my-1">Clean</button>
+                            <button onClick={handleShow} type="button" className="btn btn-success my-1">Clean</button>
                             <button type="button" className="btn btn-success my-1">Open</button>
                         </div>
                         <div className={styles.AttendanceRegister}>
@@ -30,9 +72,9 @@ const CareTakerDashboard=()=>{
                         </div>
                    </div>
                    <div className={styles.card}>
-
+                               
                    </div>
-                   <div className={styles.card}>
+                   <div id="centerCard2" className={styles.card}>
 
                    </div>
                    <div className={styles.card}>
